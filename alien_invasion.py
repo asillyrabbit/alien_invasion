f6356f882a13865ae51d7820ebb99b1dfea31820
niejun <jun.nie@icloud.com>
2021-03-18 18:09:33 +0800

    03月18号提交。

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -16,7 +18,8 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
         # 全屏模式
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -24,6 +27,9 @@ class AlienInvasion:
         # self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
+
+        # 创建一个用户存储游戏统计信息的实例。
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -83,6 +89,18 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """响应子弹和外星人碰撞。"""
+        # 删除发送碰撞的子弹和外星人。
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # 删除现有的所有子弹，并创建一群新的外星人。
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """
         检查是否有外星人位于屏幕边缘，
@@ -90,6 +108,27 @@ class AlienInvasion:
         """
         self._check_fleet_edges()
         self.aliens.update()
+
+        # 检测外星人与飞船之间的碰撞。
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+
+    def _ship_hit(self):
+        """响应飞船被外星人撞到。"""
+
+        # 将ships_left减1。
+        self.stats.ship_left -= 1
+
+        # 清空余下的外星人和子弹。
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # 创建一群新的外星人，并将飞船放到屏幕底端的中央。
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # 暂停。
+        sleep(0.5)
 
     def _check_fleet_edges(self):
         """有外星人到达边缘时采取相应的措施。"""
